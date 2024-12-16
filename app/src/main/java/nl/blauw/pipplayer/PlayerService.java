393de3e4c910d5d237159ec17ca754a12c7916b1
Cheol Jeon <56591823+nieuwmijnleven@users.noreply.github.com>
2024-12-16 18:12:27 +0900

    Update PlayerService.java

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.java
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.java
@@ -78,9 +78,11 @@ public class PlayerService extends Service {
                 params.type = WindowManager.LayoutParams.TYPE_TOAST;
 	   	 }
        	
-		    params.gravity = Gravity.TOP | Gravity.LEFT;
+	    params.gravity = Gravity.TOP | Gravity.LEFT;
             params.x = 100;
             params.y = 200;
+            params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+	    params.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
             //simpleExoPlayerView = new StyledPlayerView(getApplicationContext());
             simpleExoPlayerView = new PlayerView(getApplicationContext());
