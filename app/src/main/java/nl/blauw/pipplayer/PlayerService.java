573f6072f0218eda5f87cbc72f76171c3c356839
Cheol Jeon <56591823+nieuwmijnleven@users.noreply.github.com>
2024-12-17 15:32:26 +0900

    Update PlayerService.java

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.java
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.java
@@ -369,9 +369,9 @@ if (videoWidth > 0 && videoHeight > 0) {
         params.width = actualWidth;
         params.height = actualHeight;
         //binding.surfaceView.setHeights(popupLayoutParams.height, popupLayoutParams.height);
-        //windowManager.updateViewLayout(simpleExoPlayerView, params);
+        windowManager.updateViewLayout(simpleExoPlayerView, params);
 
-	{
+	/*{
            ViewGroup.LayoutParams params = simpleExoPlayerView.getLayoutParams();
         //params.width = ViewGroup.LayoutParams.MATCH_PARENT;
         //params.height = (int) ((float) width / height * params.width);
@@ -380,7 +380,7 @@ if (videoWidth > 0 && videoHeight > 0) {
         //if (simpleExoPlayerView != null) simpleExoPlayerView.setLayoutParams(params);
 	simpleExoPlayerView.setLayoutParams(params);
 
-	}
+	}*/
         
         return true;
     }
