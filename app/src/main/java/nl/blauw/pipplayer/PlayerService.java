a55cadf4dbb9c58b31df01871cbedf980567dc1a
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-16 08:30:52 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.java
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class PlayerService extends Service {
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     private PlayerManager playerManager;
     private PopupManager popupManager;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,6 +24,13 @@ public class PlayerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+      Notification notification = new Notification.Builder(this, CHANNEL_ID)
+            .setContentTitle("PopupPlayer Service 실행 중")
+            .setContentText("Foreground Service가 실행 중입니다.")
+            .setSmallIcon(android.R.drawable.ic_menu_info_details)
+            .build();
+        startForeground(1, notification);
+      
         String url = intent.getStringExtra("data");
 
         // PlayerManager와 PopupManager 초기화 및 실행
@@ -38,5 +52,20 @@ public class PlayerService extends Service {
         if (playerManager != null) playerManager.releasePlayer();
         if (popupManager != null) popupManager.removePopupWindow();
         super.onDestroy();
+    }
+    
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "PopupPlayer Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+        }
     }
 }
