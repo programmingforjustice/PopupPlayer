a29233c01431c59e0a5267176681e732be4f662b
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-16 08:39:29 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.java
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.java
@@ -1,8 +1,12 @@
 
 package nl.blauw.pipplayer;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 public class PlayerService extends Service {
