2ab3299d4038cdeba60d211e7916ec3122ce88d4
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-21 04:11:07 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/MainActivity.java
+++ app/src/main/java/nl/blauw/pipplayer/MainActivity.java
@@ -211,7 +211,7 @@ public class MainActivity extends AppCompatActivity {
       startActivityForResult(request, REQUEST_CODE_OVERLAY_PERMISSION);
     } else {
       Intent intent = new Intent(this, PlayerService.class);
-      intent.putExtra("data", url);
+      intent.putExtra("url", url);
       startForegroundService(intent);
     }
   }
