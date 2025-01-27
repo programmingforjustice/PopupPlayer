fab74aec179493ae1983d8e105178db81218e43e
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-27 05:33:54 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.kt
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.kt
@@ -4,10 +4,12 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.view.WindowManager
+import android.graphics.PixelFormat
 import android.widget.Toast
 import org.json.JSONArray
 import org.json.JSONObject
@@ -110,7 +112,7 @@ class PlayerService : Service() {
                   WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
               else
                   WindowManager.LayoutParams.TYPE_TOAST
-              flags = DEFAULT_WINDOW_FLAGS
+              flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
               format = PixelFormat.TRANSLUCENT
             }
             
