c49dafcff56c5fdc64662cc55186fc297dd13783
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-27 05:27:13 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerService.kt
+++ app/src/main/java/nl/blauw/pipplayer/PlayerService.kt
@@ -97,7 +97,7 @@ class PlayerService : Service() {
               }            
               
             val player = playerController.getPlayerManager().getPlayer()
-            player.currentPosition = playerInfo.getLong("currentPosition")
+            player.seekTo(playerInfo.getLong("currentPosition"))
             player.isPlaying = playerInfo.getBoolean("isPlaying")
             
             val layoutParams = WindowManager.LayoutParams().apply {
