fd480000576c492614a813a3cec3097d86d6bdc4
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-01-28 05:13:35 +0000

    update

---
+++ app/src/main/java/nl/blauw/pipplayer/PlayerViewWrapper.kt
@@ -0,0 +1,43 @@
+package nl.blauw.pipplayer
+
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
+import android.view.TextureView
+import android.widget.ImageButton
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerView
+
+class PlayerViewWrapper(private val context: Context): PlayerView(context) {
+    
+    fun setKeepScreenOn(keepScreenOn: Boolean) = this.keepScreenOn = keepScreenOn
+
+    fun setControllerShowTimeoutMs(timeoutMs: Int) = this.controllerShowTimeoutMs = timeoutMs
+
+    fun setRepeatToggleModes(modes: Int) = 
+        this.setRepeatToggleModes(modes)
+    
+    fun setupCrossButton(action: (View) -> Unit) {
+        val crossButton: ImageButton? = this.findViewById(R.id.cross_button)
+        crossButton?.setOnClickListener(action)
+    }
+    
+    fun setupMuteToggleButton(action: (View) -> Unit) {
+        val muteToggleButton: ImageButton? = this.findViewById(R.id.mute_toggle_button)
+        muteToggleButton?.setOnClickListener(action)
+    }
+
+    fun setupTouchListener(action: (View, MotionEvent) -> Boolean) {
+        this.setOnTouchListener(action)
+    }
+    
+    /*fun applyToPlayerView(command: (playerView: PlayerView) -> Unit) {
+        command(playerView)
+    }*/
+
+    //fun getPlayerView(): PlayerView = playerView
+
+    /*fun releasePlayerView() {
+        this.player = null
+    }*/
+}
