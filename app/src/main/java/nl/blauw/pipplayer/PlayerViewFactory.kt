a8739a1f85b00f0267a7e5f9d82f4dfd711ba257
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-02-01 07:59:25 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerViewFactory.kt
+++ app/src/main/java/nl/blauw/pipplayer/PlayerViewFactory.kt
@@ -22,13 +22,13 @@ class DefaultPlayerViewFactory(
             //val videoSurfaceView = playerView.videoSurfaceView
 
         // videoSurfaceView가 SurfaceView인 경우에만 Z-Order를 최상위로 설정
-        if (videoSurfaceView is SurfaceView) {
+        //if (videoSurfaceView is SurfaceView) {
             // Surface를 최상위 레이어로 올림
             videoSurfaceView.setZOrderOnTop(true)
             // 투명/반투명 처리를 가능하게 하려면 PixelFormat 변경
             videoSurfaceView.holder.setFormat(PixelFormat.TRANSLUCENT)
             this.videoSurfaceView?.setBackgroundColor(Color.WHITE) // 원하는 색상으로 변경
-            }
+            //}
         }
     }
 }
