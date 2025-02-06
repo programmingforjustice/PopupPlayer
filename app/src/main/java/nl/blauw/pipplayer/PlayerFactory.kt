13d4ea7f32720a2bf77a7fd86382fafd25306431
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-02-06 05:43:24 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PlayerFactory.kt
+++ app/src/main/java/nl/blauw/pipplayer/PlayerFactory.kt
@@ -45,10 +45,17 @@ class DefaultPlayerFactory(
         val dataSourceFactory = DefaultDataSourceFactory(
             context, Util.getUserAgent(context, context.getString(R.string.app_name))
         )
+        
+        val mediaItem = MediaItem.Builder()
+            .setUri(contentUrl)
+            .setImageDurationMs(5000) // 이미지 표시 시간: 5000ms (5초)
+            .build()
+    
         val contentMediaSource: MediaSource = ProgressiveMediaSource.Factory(
             dataSourceFactory,
             DefaultExtractorsFactory()
-        ).createMediaSource(MediaItem.fromUri(contentUrl))
+        ).createMediaSource(mediaItem))
+        //.createMediaSource(MediaItem.fromUri(contentUrl))
 
         (player as PlayerWrapper).apply {
           enableExoPlayerFeatures().setMediaSource(contentMediaSource)
