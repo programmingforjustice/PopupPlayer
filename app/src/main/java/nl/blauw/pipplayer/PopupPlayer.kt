a572c3fca7af99c228b48351eb163bbd20917eab
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-02-07 07:46:57 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
@@ -53,18 +53,14 @@ abstract class PopupPlayer(protected val context: Context) {
     }
 
     companion object {
-        @JvmStatic
-        protected const val MAX_POPUP_WIDTH = 400
-        @JvmStatic
-        protected const val MAX_POPUP_HEIGHT = 400
-        @JvmStatic
-        protected const val DEFAULT_POPUP_X = 100
-        @JvmStatic
-        protected const val DEFAULT_POPUP_Y = 200
-        @JvmStatic
-        protected const val CONTROLLER_SHOW_TIMEOUT = 2500
-        @JvmStatic
-        protected const val DEFAULT_WINDOW_FLAGS = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        const val MAX_POPUP_WIDTH = 400
+        const val MAX_POPUP_HEIGHT = 400
+
+        const val DEFAULT_POPUP_X = 100
+        const val DEFAULT_POPUP_Y = 200
+
+        const val CONTROLLER_SHOW_TIMEOUT = 2500
+        const val DEFAULT_WINDOW_FLAGS = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
     }
 
     fun show(params: WindowManager.LayoutParams? = null) {
