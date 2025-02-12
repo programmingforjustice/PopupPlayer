39ff3c88bd603d3eb991bed36f311adb51afc74d
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-02-12 02:28:24 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
@@ -362,6 +362,8 @@ class VideoPopupPlayer @JvmOverloads constructor(context: Context, private val c
             }
         
             windowManager.updateViewLayout(playerView, fullscreenParams)
+            removePopupWindow()
+            windowManager.updateViewLayout(playerView, fullscreenParams)
             setupImmersiveMode()
         }
     }
