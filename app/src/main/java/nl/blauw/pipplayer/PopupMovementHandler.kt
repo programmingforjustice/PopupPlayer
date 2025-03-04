465a71cc9dd877ebb59307652b8e2c8067cbf55e
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-03-04 03:09:24 +0000

    update

---
+++ app/src/main/java/nl/blauw/pipplayer/PopupMovementHandler.kt
@@ -0,0 +1,59 @@
+package nl.blauw.pipplayer
+
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.os.Bundle
+import android.util.DisplayMetrics
+
+class PopupMovementHandler(
+    context: Context,
+    private val windowManager: WindowManager,
+    private val params: WindowManager.LayoutParams
+) : View.OnTouchListener {
+
+    private val displayWidth
+    private val displayHeight
+    private var offsetX = 0f
+    private var offsetY = 0f
+    private var flagActionMove = false
+    
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics: WindowMetrics = windowManager.currentWindowMetrics
+            val bounds = windowMetrics.bounds
+            displayWidth = bounds.width()
+            displayHeight = bounds.height()
+        } else {
+            val displayMetrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(displayMetrics)
+            displayWidth = displayMetrics.widthPixels
+            displayHeight = displayMetrics.heightPixels
+        }
+    }
+
+    override fun onTouch(view: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                flagActionMove = false
+                offsetX = minOf(maxOf(0, params.x - event.rawX), displayWidth)
+                offsetY = minOf(maxOf(0, params.y - event.rawY), displayHeight)
+                return false
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                flagActionMove = true
+                params.x = minOf(maxOf(0, (event.rawX + offsetX).toInt()), displayWidth)
+                params.y = minOf(maxOf(0, (event.rawY + offsetY).toInt()), displayHeight)
+                windowManager.updateViewLayout(view, params)
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                return flagActionMove
+            }
+        }
+        return false
+    }
+}
