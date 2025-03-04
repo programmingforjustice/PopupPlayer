40f881b3b9681b14abbe9e4f9a088362f7a948cb
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-03-04 03:25:06 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupMovementHandler.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupMovementHandler.kt
@@ -5,6 +5,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.os.Bundle
+import android.os.Build
 import android.util.DisplayMetrics
 
 class PopupMovementHandler(
