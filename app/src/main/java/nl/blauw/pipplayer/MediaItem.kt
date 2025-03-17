912d647ddec3e235e8bd90d12721630e6f5b6afa
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-03-17 23:44:34 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/MediaItem.kt
+++ app/src/main/java/nl/blauw/pipplayer/MediaItem.kt
@@ -1,6 +1,7 @@
 package nl.blauw.pipplayer
 
 import android.net.Uri
+import android.graphics.Bitmap
 
 /*data class MediaItem(
     val uri: Uri,
@@ -12,6 +13,7 @@ data class MediaItem(
     val id: Long,
     val displayName: String,
     val mimeType: String,
+    val mediaType: Int,
     val contentUri: Uri,
     var thumbnail: Bitmap? = null
 )
