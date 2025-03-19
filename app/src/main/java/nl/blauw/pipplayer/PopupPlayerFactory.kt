e0b7ac42267a5f9265fdf8343f55c855a1ebf1fa
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-03-19 09:21:55 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayerFactory.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayerFactory.kt
@@ -82,7 +82,8 @@ class VideoPopupPlayerFactory(context: Context) : PopupPlayerFactory(context) {
     
     override fun create(mediaUrl: String): PopupPlayer {
         //return VideoPopupPlayer(context, mediaUrl)
-        return BasicVideoPopupPlayer(context, mediaUrl)
+        //return BasicVideoPopupPlayer(context, mediaUrl)
+        return AdaptivePopupPlayer(context, mediaUrl)
         
     }
 }
