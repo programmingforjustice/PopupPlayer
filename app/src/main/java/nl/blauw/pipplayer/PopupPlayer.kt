05fc63121e05676705f143fefb1cb75eb89de378
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-03-20 10:56:03 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
@@ -472,9 +472,9 @@ class AdaptivePopupPlayer @JvmOverloads constructor(context: Context, private va
             setOnIsPlayingChangedListener {
                 var imagePopupPlayer = ImagePopupPlayer(context, null, this.exportCurrentFrame()).apply {
                         setOnClickListener {
-                          this@AdaptivePopupPlayer.popupPlayer.dispose()
+                       var layoutParams = this@AdaptivePopupPlayer.popupPlayer.layoutParams this@AdaptivePopupPlayer.popupPlayer.dispose()
                         createPopupWindow()
-                        this@AdaptivePopupPlayer.popupPlayer.show()
+                        this@AdaptivePopupPlayer.popupPlayer.show(layoutParams)
                             this@AdaptivePopupPlayer.popupPlayer.play()
                             
                         }
