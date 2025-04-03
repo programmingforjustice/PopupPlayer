fdd028c652e1e45e5154b853af7f7b1b83aea897
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-04-03 06:20:44 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupMovementHandler.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupMovementHandler.kt
@@ -26,66 +26,23 @@ class PopupMovementHandler(
     private var offsetX = 0f
     private var offsetY = 0f
     private var flagActionMove = false
-    
-    /*private val displayManager: DisplayManager = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
 
-    private val displayListener = object : DisplayManager.DisplayListener {
-        override fun onDisplayChanged(displayId: Int) {
-            Toast.makeText(context, "displayId: $displayId", Toast.LENGTH_SHORT).show()
-            if (displayId == Display.DEFAULT_DISPLAY) {
-                updateDisplaySettings()
-                // println("화면 회전 감지됨: $rotation")
-                //calculateDisplayResolution()
-                Toast.makeText(context, "화면 회전 감지됨: ($displayWidth, $displayHeight)", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        override fun onDisplayAdded(displayId: Int) {}
-        override fun onDisplayRemoved(displayId: Int) {}
-    }*/
-    
     init {
-        //displayManager.registerDisplayListener(displayListener, null)
         updateDisplaySettings()
-        
     }
     
     fun updateDisplaySettings() {
-        // offsetX = 0f
-        // offsetY = 0f
-        // flagActionMove = false
-        
-        val (width, height) = getDisplayResolution()
-        displayWidth = width
-        displayHeight = height
-        
-        /*val rotation = windowManager.defaultDisplay.rotation
-        when (rotation) {
-            Surface.ROTATION_0 ->  { 
-                Toast.makeText(context, "화면 회전 감지됨: Surface.ROTATION_0", Toast.LENGTH_SHORT).show()
-            }
-            Surface.ROTATION_90 -> {
-                Toast.makeText(context, "화면 회전 감지됨: Surface.ROTATION_90", Toast.LENGTH_SHORT).show()
-            }
-            Surface.ROTATION_180 -> { 
-                Toast.makeText(context, "화면 회전 감지됨: Surface.ROTATION_180", Toast.LENGTH_SHORT).show()
-            }
-            Surface.ROTATION_270 -> {
-                Toast.makeText(context, "화면 회전 감지됨: Surface.ROTATION_270", Toast.LENGTH_SHORT).show()
-            }
-            //else -> -1 // 알 수 없는 값
-        }*/
+        getDisplayResolution().also { (width, height) -> 
+            displayWidth = width
+            displayHeight = height
+        }
     }
     
     fun getDisplayResolution(): Pair<Int, Int> {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {  
-            // API 30 이상 (Android 11+)
-            //val windowManager = getSystemService(WindowManager::class.java)
             val metrics: WindowMetrics = windowManager.maximumWindowMetrics
             Pair(metrics.bounds.width(), metrics.bounds.height())
         } else {  
-            // API 29 이하 (Android 10-)
-            //val windowManager = getSystemService(WindowManager::class.java)
             val display: Display = windowManager.defaultDisplay
             val size = Point()
             display.getRealSize(size)
