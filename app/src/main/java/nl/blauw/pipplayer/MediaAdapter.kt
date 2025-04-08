3617cb82121ebc7b096a505ba085510d4ca7a56a
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-04-08 03:46:27 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/MediaAdapter.kt
+++ app/src/main/java/nl/blauw/pipplayer/MediaAdapter.kt
@@ -2,6 +2,7 @@ package nl.blauw.pipplayer
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.content.ContentResolver
 import androidx.recyclerview.widget.RecyclerView
 import nl.blauw.pipplayer.databinding.ItemFileBinding
 
@@ -12,7 +13,7 @@ class MediaAdapter(private val items: List<MediaItem>) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(mediaItem: MediaItem) {
             binding.mediaPath.text = mediaItem.displayName
-            val thumbnail = contentResolver.loadThumbnail(item.contentUri, Size(200, 200), null)
+            val thumbnail = contentResolver.loadThumbnail(mediaItem.contentUri, Size(200, 200), null)
             binding.imageThumbnail.setImageBitmap(thumbnail)
             // 필요시, 미디어 타입에 따라 아이콘이나 썸네일 로딩 등을 추가할 수 있습니다.
         }
