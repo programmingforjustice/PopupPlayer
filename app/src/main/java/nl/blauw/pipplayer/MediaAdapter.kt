735dd022be2f8aa7462f9e821f647bc93960b58f
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-04-08 03:44:46 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/MediaAdapter.kt
+++ app/src/main/java/nl/blauw/pipplayer/MediaAdapter.kt
@@ -12,7 +12,8 @@ class MediaAdapter(private val items: List<MediaItem>) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(mediaItem: MediaItem) {
             binding.mediaPath.text = mediaItem.displayName
-            binding.imageThumbnail.setImageBitmap(mediaItem.thumbnail)
+            val thumbnail = contentResolver.loadThumbnail(item.contentUri, Size(200, 200), null)
+            binding.imageThumbnail.setImageBitmap(thumbnail)
             // 필요시, 미디어 타입에 따라 아이콘이나 썸네일 로딩 등을 추가할 수 있습니다.
         }
     }
