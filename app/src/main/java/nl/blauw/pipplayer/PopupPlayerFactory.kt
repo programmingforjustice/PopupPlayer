a10c6360cb06d7905a26d914180ed5506ae7726d
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-04-12 05:41:13 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayerFactory.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayerFactory.kt
@@ -1,6 +1,7 @@
 
 package nl.blauw.pipplayer
 
+import android.net.Uri
 import android.content.Context
 import android.os.Build
 import android.graphics.PixelFormat
@@ -76,7 +77,7 @@ class VideoPopupPlayerFactory(context: Context) : PopupPlayerFactory(context) {
     }
     
     override fun canHandle(mediaUrl: String): Boolean {
-        val mimeType = context.contentResolver.getType(mediaUrl)?
+        val mimeType = context.contentResolver.getType(Uri.parse(mediaUrl))
         return mimeType?.split("/").getOrNull(1)?.lowercase()?.let { subType -> 
                 subType.endsWith(".mp4") 
                 || subType.endsWith(".mkv")
