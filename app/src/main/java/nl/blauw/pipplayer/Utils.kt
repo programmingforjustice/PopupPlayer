ef6bad3b04e2e5af1715cc0ca1b7640df244abd9
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-04-15 08:52:55 +0000

    update

--- app/src/main/java/nl/blauw/pipplayer/Utils.kt
+++ app/src/main/java/nl/blauw/pipplayer/Utils.kt
@@ -46,6 +46,7 @@ fun loadBitmapFromStorage(context: Context, uri: Uri): Bitmap? {
 }
 
 fun loadBitmap(context: Context, uri: Uri): Bitmap? {
+    debug(context, uri.toString())
    return when(uri.scheme) {
        "content" -> loadBitmapFromContentUri(context, uri)
        "file", null -> loadBitmapFromStorage(context, uri)
