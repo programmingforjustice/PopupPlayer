b8aaace169f7b80913784051c1f3c0c074a0d345
nieuwmijnleven <nieuwmijnleven@outlook.com>
2025-05-01 02:04:12 +0000

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+# 첫 번째 인자를 가져옴
+MODE=$1
+
+# 인자가 비었는지 확인
+if [ -z "$MODE" ]; then
+  echo "사용법: $0 [dev|prod]"
+  exit 1
+fi
+
 git pull
-./gradlew clean assembleDevRelease --rerun-tasks
 
-
+# 분기 처리
+case "$MODE" in
+  dev)
+    ./gradlew clean assembleDevRelease --rerun-tasks
+    ;;
+  prod)
+    ./gradlew clean assembleProdRelease --rerun-tasks
+    ;;
+  *)
+    echo "오류: dev 또는 prod만 입력하세요."
+    exit 1
+    ;;
+esac
--- fetchApk.sh
+++ fetchApk.sh
@@ -1,11 +1,33 @@
-#!/usr/bin/expect
+#!/bin/expect
 
 # 명령어 경로 설정 (필요 시 수정)
 set env(PATH) "/usr/local/bin:/usr/bin:/bin:$env(PATH)"
 
-# 명령어 실행
+# 현재 스크립트 파일 이름 (자기 자신)
+set script_name [file tail $argv0]
+
+# 인자 개수 확인
+if { $argc != 1 } {
+    puts "사용법: ./$script_name [dev|prod]"
+    exit 1
+}
+
+# 첫 번째 인자 가져오기
+set mode [lindex $argv 0]
 set timeout -1
-spawn gh cs cp -e remote:/workspaces/PopupPlayer/app/build/outputs/apk/release/app-release.apk /root/PopupPlayer
+
+# 분기 처리
+if { $mode == "dev" } {
+    spawn gh cs cp -e remote:/workspaces/PopupPlayer/app/build/outputs/apk/dev/release/app-dev-release.apk /root/storage/shared/Apks
+    #interact
+} elseif { $mode == "prod" } {
+    spawn gh cs cp -e remote:/workspaces/PopupPlayer/app/build/outputs/apk/prod/release/app-prod-release.apk /root/storage/shared/Apks
+    #interact
+} else {
+    puts "오류: dev 또는 prod만 입력하세요."
+    puts "사용법: ./$script_name [dev|prod]"
+    exit 1
+}
 
 # 엔터 입력 자동화
 expect {
@@ -13,5 +35,5 @@ expect {
     eof
 }
 
-spawn cp /root/PopupPlayer/app-release.apk /root/storage/shared/Apks/app-release-refactoring.apk
-expect eof
+#spawn cp /root/PopupPlayer/app-release.apk /root/storage/shared/Apks/app-release-refactoring.apk
+#expect eof
