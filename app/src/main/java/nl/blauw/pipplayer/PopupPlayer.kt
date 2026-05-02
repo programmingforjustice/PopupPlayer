67021833b0175079ee22dd948acece5f8fb4f89f
Cheol Jeon <nieuwmijnleven@outlook.com>
2026-05-02 13:04:08 +0200

    update

--- app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
+++ app/src/main/java/nl/blauw/pipplayer/PopupPlayer.kt
@@ -89,7 +89,7 @@ abstract class BasePopupPlayer(protected val context: Context) : PopupPlayer {
         val view = popupPlayerView ?: return
         isGhostMode = !isGhostMode
         if (isGhostMode) {
-            view.alpha = 0.2f
+            view.alpha = 0.5f
             layoutParams.alpha = 0.5f
             layoutParams.flags = layoutParams.flags or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
             windowManager.updateViewLayout(view, layoutParams)
